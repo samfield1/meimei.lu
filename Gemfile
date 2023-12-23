a77a4508902b343f4fad694f5d8caf1f8b03f6c6
Sam Mansfield <sammansfield1@gmail.com>
2023-12-23 13:31:26 -0500

    Everything but images

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+gem 'github-pages'
+group :jekyll_plugins do
+    gem "webrick", "~> 1.8"
+end
---
+++ convert-img.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Simple script to convert all images to webp
+# The images are too large, so I need them to be webp before uploading to GitHub
+
+# Only executes in project base directory
+cd $(dirname $(basename $0))
+
+if [[ -n $1 ]]; then
+    QUALITY=$1
+else
+    QUALITY=95
+fi
+
+images=$(find assets/og-img/ | grep -E ".*\.(jpg|gif|png|jpeg)$")
+images=($images)
+images_n=${#images[*]}
+for (( i = 0; i < images_n; i++ )); do
+    in_image=${images[$i]}
+    out_image=${in_image//"/og-img"/}
+    out_image=${out_image%.*}.webp
+
+    mkdir -p $(dirname $out_image)
+    echo "($((i+1))/$images_n) Coverting $in_image"
+    cwebp -q $QUALITY $in_image -o $out_image >/dev/null 2>&1
+done
+
+echo "Completed image conversion."
