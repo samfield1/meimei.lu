e525d8d3dbbec03da0ba991375c5ffe59413349a
Meimei Lu <meimeilu@gwu.edu>
2024-01-06 14:07:08 -0500

    Mac compatibility edits

--- convert-img.sh
+++ convert-img.sh
@@ -5,23 +5,32 @@
 # Only executes in project base directory
 cd $(dirname $(basename $0))
 
-if [[ -n $1 ]]; then
-    QUALITY=$1
-else
-    QUALITY=100
-fi
+quality=100
+overwrite=false
+while getopts ":q:f" opt; do
+    case $opt in
+    q) quality=$OPTARG;;
+    f) overwrite=true;;
+    ?) exit 1;;
+    esac
+done
 
-images=$(find assets/og-img/ | grep -E ".*\.(jpg|gif|png|jpeg)$")
+
+images=$(find assets/og-img | grep -E ".*\.(jpg|gif|png|jpeg)$")
 images=($images)
 images_n=${#images[*]}
 for (( i = 0; i < images_n; i++ )); do
     in_image=${images[$i]}
-    out_image=${in_image//"/og-img"/}
+    out_image=$(sed -e 's/\/og-img//g' <<<$in_image)
     out_image=${out_image%.*}.webp
 
     mkdir -p $(dirname $out_image)
-    echo "($((i+1))/$images_n) Coverting $in_image"
-    cwebp -q $QUALITY $in_image -o $out_image >/dev/null 2>&1
+    if [[ -s $out_image && ! $overwrite ]]; then
+        echo "($((i+1))/$images_n) Skipping $in_image (webp exists)"
+    else
+        echo "($((i+1))/$images_n) Coverting $in_image"
+        cwebp -q $quality $in_image -o $out_image >/dev/null 2>&1
+    fi
 done
 
 echo "Completed image conversion."
